1820ae31b27515ecb856d86d9313cc9088c2f34d
emmacor26323 <emmacoronado08@gmail.com>
2026-08-23 00:13:02 -0600

    Update Usuario.java
    Declaración de atributos de la clase Usuario.
    Eliminación de comentarios inecesarios creados al generar el código a base del UML.

--- BiblioMath/src/Usuario.java
+++ BiblioMath/src/Usuario.java
@@ -2,248 +2,141 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
+
 public class Usuario {
-
-    /**
-     * Default constructor
-     */
-    public Usuario() {
-    }
-
-    /**
-     * 
-     */
     private String nombre;
-
-    /**
-     * 
-     */
     private String contrasena;
-
-    /**
-     * 
-     */
-    private Set<Conceptos> baseConceptos;
-
-    /**
-     * 
-     */
+    private ArrayList<Conceptos> baseConceptos;
     private String aConceptos;
-
-    /**
-     * 
-     */
-    private Set<Kits> baseKits;
-
-    /**
-     * 
-     */
-    public String aKits;
-
-    /**
-     * 
-     */
+    private ArrayListe<Kits> baseKits;
+    private String aKits;
     private String ultimaVista;
 
-
-
-
-
-    /**
-     * 
-     */
+    
     public Usuario() {
-        // TODO implement here
+       
     }
 
-    /**
-     * @param String direccion 
-     * @param Array local
-     */
-    public void BajarBaseDatos(void String direccion, void Array local) {
-        // TODO implement here
+   
+    public void BajarBaseDatos(String direccion, ArrayList local) {
+    
     }
 
-    /**
-     * @param String direccion 
-     * @param Array local
-     */
-    public void GuardarBaseDatos(void String direccion, void Array local) {
-        // TODO implement here
+    
+    public void GuardarBaseDatos(String direccion, ArrayList local) {
+       
     }
 
-    /**
-     * @param nombre
-     */
+   
     public void setNombre(String nombre) {
-        // TODO implement here
+       
     }
 
-    /**
-     * @return
-     */
+    
     public String getNombre() {
-        // TODO implement here
         return "";
     }
 
-    /**
-     * @param contrasena
-     */
+    
     public void setContrasena(String contrasena) {
-        // TODO implement here
+        
     }
 
-    /**
-     * @return
-     */
     public String getContrasena() {
-        // TODO implement here
         return "";
     }
 
-    /**
-     * @param baseConceptos
-     */
     public void setBaseConceptos(Set<Conceptos> baseConceptos) {
-        // TODO implement here
+        
     }
 
-    /**
-     * @return
-     */
+  
     public Set<Conceptos> getBaseConceptos() {
-        // TODO implement here
         return null;
     }
 
-    /**
-     * @param baseKits
-     */
+
     public void setBaseKits(Set<Kits> baseKits) {
-        // TODO implement here
+
     }
 
-    /**
-     * @return
-     */
+    
     public Set<Kits> getBaseKits() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @param ultimaVisita
-     */
+   
     public void setUltimaVista(String ultimaVisita) {
-        // TODO implement here
+        
     }
 
-    /**
-     * @return
-     */
+
     public String getUltimaVista() {
-        // TODO implement here
+       
         return "";
     }
 
-    /**
-     * @return
-     */
+ 
     public String getAConceptos() {
-        // TODO implement here
+      
         return "";
     }
 
-    /**
-     * @return
-     */
+    
     public String getAKit() {
-        // TODO implement here
+        
         return "";
     }
 
-    /**
-     * @return
-     */
     public void nuevoConcepto() {
-        // TODO implement here
+        
         return null;
     }
 
-    /**
-     * @return
-     */
+    
     public String motrarConceptos() {
-        // TODO implement here
+        
         return "";
     }
 
-    /**
-     * @return
-     */
+   
     public String buscarConcepto() {
-        // TODO implement here
+        
         return "";
     }
 
-    /**
-     * @param Curso 
-     * @return
-     */
+  
     public void filtrarCurso(String Curso) {
-        // TODO implement here
+       
         return null;
     }
 
-    /**
-     * @param Categoria 
-     * @return
-     */
+   
     public void filtrarCategoria(String Categoria) {
-        // TODO implement here
+        
         return null;
     }
 
-    /**
-     * @return
-     */
+    
     public void creatKitStudio() {
-        // TODO implement here
+        
         return null;
     }
 
-    /**
-     * @return
-     */
     public List <String> mostrarKits() {
-        // TODO implement here
         return null;
     }
 
-    /**
-     * @param nombre
-     */
+    
     public void buscarKits(String nombre) {
-        // TODO implement here
+        
     }
 
-    /**
-     * @return
-     */
+  
     public void irUltimo() {
-        // TODO implement here
+        
         return null;
-    }
-
-    /**
-     * 
-     */
-    public void Operation3() {
-        // TODO implement here
     }
 
 }
