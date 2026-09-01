fc258d6a962862d8c789e90bce0f1ed59a4e4f77
Elisa Menéndez <men26591@uvg.edu.gt>
2026-09-01 11:43:05 -0600

    Commit 2.2

--- BiblioMath/src/Biblioteca.java
+++ BiblioMath/src/Biblioteca.java
@@ -17,6 +17,17 @@ public class Biblioteca {
     public Usuario getUsuarioActual() {
         return usuarioActual;
     }
-}
 
+    public String crearUsuario(String nombre, String contrasena) {
+
+        Usuario nuevoUsuario = new Usuario();
+
+        nuevoUsuario.setNombre(nombre);
+        nuevoUsuario.setContrasena(contrasena);
+
+        baseUsuarios.add(nuevoUsuario);
+
+        return "Usuario creado correctamente";
+    }
+}
 
