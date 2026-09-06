aec852e90c47bf753434884381f7e235ded695e1
Andrea Chamale <cha26770@uvg.edu.gt>
2026-09-06 10:53:15 -0600

    Corrección Kit
    Eliminé algo que Victor había hecho, entonces lo volví a poner.

--- BiblioMath/src/Kit.java
+++ BiblioMath/src/Kit.java
@@ -44,11 +44,27 @@ public class Kit {
                 conceptos.add(concepto);
         }
     }
-
+    
+    public void mostrarKit(){
+        System.out.println("Kit: " + nombre);
+        for(int i = 0; i < conceptos.size(); i++){
+            System.out.println(conceptos.get(i));
+        }
+    }
+    
+    public Concepto buscarenkit(int id){
+        for(int i = 0; i < conceptos.size(); i++){
+            if(id == conceptos.get(i).getId()){
+                return conceptos.get(i);
+            }
+        }
+        return null;
+    
     public String toString(){ 
 
         return nombre + ","+ conceptos;
 
     }
+
 
 }
