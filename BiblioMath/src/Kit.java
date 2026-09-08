c39605f75f7880ee71803f52bf53b0804d8f56f9
JVCA247 <cha26605@uvg.edu.gt>
2026-09-08 12:34:09 -0600

    Corrección 1
    Elimine un método innecesario

--- BiblioMath/src/Kit.java
+++ BiblioMath/src/Kit.java
@@ -53,17 +53,6 @@ public class Kit {
             } 
         }
     }
-    
-    /* public Concepto buscarenkit(int id){
-        for(int i = 0; i < conceptos.size(); i++){
-            if(conceptos.get(i) != null){
-                if(id == conceptos.get(i).getId()){
-                    return conceptos.get(i);
-                }
-            }          
-        }
-        return null;
-    }
     public void cambiarConcepto(int id, Concepto concepto){
         for(int i = 0; i < conceptos.size(); i++){
            if(conceptos.get(i) != null){
@@ -72,7 +61,7 @@ public class Kit {
                 }
             } 
         }
-    }   */
+    }
     public String toString(){ 
         return nombre + ","+ conceptos;
     }
